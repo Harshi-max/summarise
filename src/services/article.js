73770104aa0d3a36d71cf018c8f7865d199a28ed
Harshi31-g <srilakshmidevimutyala@gmail.com>
2025-06-23 14:54:31 +0530

    some new changes

--- src/services/article.js
+++ src/services/article.js
@@ -2,13 +2,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
+
 export const articleApi = createApi({
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key', rapidApiKey);
-            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+            headers.set('x-rapidapi-key', rapidApiKey);
+            headers.set('x-rapidapi-host', 'article-extractor-and-summarizer.p.rapidapi.com');
 
             return headers;
         },
