b28773b21cabcc2242dd266bdc468b6711a691f1
ANVSLD SRIHARSHITHA <23b01a1207@svecw.edu.in>
2025-06-27 18:27:57 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/Harshi-max/Summize", "_blank")
+            window.open("https://github.com/Harshi-max/Summarize", "_blank")
           }
           className='black_btn'
         >
